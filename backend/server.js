f74fd0c049ef805c764f2c7f0b3adb8957f6f8df
Emirhan Soylu <121471344+emomu@users.noreply.github.com>
2025-12-09 20:48:37 +0300

    limit arttirma

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // MongoDB Bağlantısı (Kendi linkin varsa burayı değiştir)
 mongoose.connect(process.env.MONGO_URI) // .env'den çekiliyor
