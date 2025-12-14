9c83388e922f02c07c56a3492b233a01f9e5b81d
Emirhan Soylu <121471344+emomu@users.noreply.github.com>
2025-12-14 06:55:13 +0300

    şifre sıfırlama iyileştirildi

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+// 1. BrowserRouter'ı import et
+import { BrowserRouter } from 'react-router-dom' 
 import { store } from './store/store'
 import './index.css'
 import App from './App.jsx'
@@ -9,9 +11,12 @@ import { ThemeProvider } from './context/ThemeContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      {/* 2. Uygulamayı BrowserRouter ile sar */}
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
